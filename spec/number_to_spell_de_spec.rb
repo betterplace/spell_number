ae23dd384bec7cdca0610f107f9b604ec40d5219
Thiago Alessio <thiagoalessio@me.com>
2012-04-02 18:08:56 -0300

    specs refactored

--- spec/number_to_spell_de_spec.rb
+++ spec/number_to_spell_de_spec.rb
@@ -1,305 +1,106 @@
 # encoding: utf-8
 require 'spell_number'
 
-describe "spell numbers in german" do  
-  
-  def test_number(number, result)
-    SpellNumber.number_to_words(number, :locale => 'de').should == result
-  end
-  
-  describe "Single digits" do
-    it "should transform 1" do
-      test_number(1, 'eins')
-    end
-  
-    it "should transform 2" do
-      test_number(2, 'zwei')
-    end
-  
-    it "should transform 3" do
-      test_number(3, 'drei')
-    end
-  
-    it "should transform 4" do
-      test_number(4, 'vier')
-    end
-  
-    it "should transform 5" do
-      test_number(5, 'fünf')
-    end
-  
-    it "should transform 6" do
-      test_number(6, 'sechs')
-    end
-  
-    it "should transform 7" do
-      test_number(7, 'sieben')
-    end
-  
-    it "should transform 8" do
-      test_number(8, 'acht')
-    end
-  
-    it "should transform 9" do
-      test_number(9, 'neun')
-    end
-  
-    it "should transform 0" do
-      test_number(0, 'null')
-    end
-  end
-  
-  describe "Teens" do 
-    
-    it "should spell 10 as zehn" do
-      test_number(10, 'zehn')
-    end
-    
-    it "should spell 11 as elf" do
-      test_number(11, 'elf')
-    end
-    
-    it "should spell 12 as zwölf" do
-      test_number(12, 'zwölf')
-    end
-    
-    it "should spell 13 as dreizehn" do
-      test_number(13, 'dreizehn')
-    end
-    
-    it "should spell 14 as vierzehn" do
-      test_number(14, 'vierzehn')
-    end
-    
-    it "should spell 15 as fünfzehn" do
-      test_number(15, 'fünfzehn')
-    end
-    
-    it "should spell 16 as sechzehn" do
-      test_number(16, 'sechzehn')
-    end
-    
-    it "should spell 17 as siebzehn" do
-      test_number(17, 'siebzehn')
-    end
-    
-    it "should spell 18 as achtzehn" do
-      test_number(18, 'achtzehn')
-    end
-    
-    it "should spell 19 as neunzehn" do
-      test_number(19, 'neunzehn')
-    end
-  end
-
-  describe "Twenties" do
-
-    it "should spell 20 as zwanzig" do
-     test_number(20, 'zwanzig')
-    end
-
-    it "should spell 21 as einundzwanzig" do
-     test_number(21, 'einundzwanzig')
-    end
-
-    it "should spell 22 as zweiundzwanzig" do
-     test_number(22, 'zweiundzwanzig')
-    end
-
-    it "should spell 23 as dreiundzwanzig" do
-     test_number(23, 'dreiundzwanzig')
-    end
-
-    it "should spell 24 as vierundzwanzig" do
-     test_number(24, 'vierundzwanzig')
-    end
-
-    it "should spell 25 as fünfundzwanzig" do
-     test_number(25, 'fünfundzwanzig')
-    end
-
-    it "should spell 26 as sechsundzwanzig" do
-     test_number(26, 'sechsundzwanzig')
-    end
-
-    it "should spell 27 as siebenundzwanzig" do
-     test_number(27, 'siebenundzwanzig')
-    end
-
-    it "should spell 28 as achtundzwanzig" do
-     test_number(28, 'achtundzwanzig')
-    end
-
-    it "should spell 29 as neunundzwanzig" do
-     test_number(29, 'neunundzwanzig')
+describe 'spell numbers in german' do
+  {
+    'Single digits' => {
+      1 => 'eins',
+      2 => 'zwei',
+      3 => 'drei',
+      4 => 'vier',
+      5 => 'fünf',
+      6 => 'sechs',
+      7 => 'sieben',
+      8 => 'acht',
+      9 => 'neun',
+      0 => 'null'
+    },
+    'Teens' => {
+      10 => 'zehn',
+      11 => 'elf',
+      12 => 'zwölf',
+      13 => 'dreizehn',
+      14 => 'vierzehn',
+      15 => 'fünfzehn',
+      16 => 'sechzehn',
+      17 => 'siebzehn',
+      18 => 'achtzehn',
+      19 => 'neunzehn'
+    },
+    'Twenties' => {
+      20 => 'zwanzig',
+      21 => 'einundzwanzig',
+      22 => 'zweiundzwanzig',
+      23 => 'dreiundzwanzig',
+      24 => 'vierundzwanzig',
+      25 => 'fünfundzwanzig',
+      26 => 'sechsundzwanzig',
+      27 => 'siebenundzwanzig',
+      28 => 'achtundzwanzig',
+      29 => 'neunundzwanzig'
+    },
+    'Thirties' => {
+      30 => 'dreissig',
+      33 => 'dreiunddreissig'
+    },
+    'Fourties' => {
+      40 => 'vierzig',
+      44 => 'vierundvierzig'
+    },
+    'Fifties' => {
+      50 => 'fünfzig',
+      55 => 'fünfundfünfzig'
+    },
+    'Sixties' => {
+      60 => 'sechzig',
+      66 => 'sechsundsechzig'
+    },
+    'Seventies' => {
+      70 => 'siebzig',
+      77 => 'siebenundsiebzig'
+    },
+    'Eighties' => {
+      80 => 'achtzig',
+      88 => 'achtundachtzig'
+    },
+    'Nineties' => {
+      90 => 'neunzig',
+      99 => 'neunundneunzig'
+    },
+    'Hundreds' => {
+      100 => 'einhundert',
+      101 => 'einhunderteins',
+      111 => 'einhundertelf',
+      155 => 'einhundertfünfundfünfzig',
+      190 => 'einhundertneunzig',
+      200 => 'zweihundert',
+      222 => 'zweihundertzweiundzwanzig',
+      999 => 'neunhundertneunundneunzig'
+    },
+    'Thousands' => {
+      1000   => 'eintausend',
+      1001   => 'eintausendeins',
+      1011   => 'eintausendelf',
+      1100   => 'eintausendeinhundert',
+      1111   => 'eintausendeinhundertelf',
+      999999 => 'neunhundertneunundneunzigtausendneunhundertneunundneunzig'
+    },
+    'Millions' => {
+      1000000      => 'eine Million',
+      1000001      => 'eine Million und eins',
+      1000011      => 'eine Million und elf',
+      1000100      => 'eine Million und einhundert',
+      1000111      => 'eine Million und einhundertelf',
+      1001111      => 'eine Million und eintausendeinhundertelf',
+      999999999999 => 'neunhundertneunundneunzigtausendneunhundertneunundneunzig Millionen und neunhundertneunundneunzigtausendneunhundertneunundneunzig'
+    }
+  }.each_pair do |description, hash|
+    send(:describe, description) do
+      hash.each_pair do |number, result|
+        send(:it, "should spell #{number} as #{result}") {
+          SpellNumber.number_to_words(number, :locale => 'de').should == result
+        }
+      end
     end
   end
-
-  describe "Thirties" do   
-    it "should spell 30 as dreissig" do
-      test_number(30, 'dreissig')
-    end
-    
-    it "should spell 33 as dreiundreissig" do
-      test_number(33, 'dreiunddreissig')
-    end    
-  end
-  
-  describe "Fourties" do  
-    it "should spell 40 as vierzig" do
-      test_number(40, 'vierzig')
-    end
-    
-    it "should spell 44 as vierundvierzig" do
-      test_number(44, 'vierundvierzig')
-    end  
-  end
-  
-  describe "Fifties" do    
-    it "should spell 50 as fünfzig" do
-      test_number(50, 'fünfzig')
-    end
-    
-    it "should spell 55 as fünfundfünfzig" do
-      test_number(55, 'fünfundfünfzig')
-    end 
-  end
-  
-  describe "Sixties" do    
-    it "should spell 60 as sechzig" do
-      test_number(60, 'sechzig')
-    end
-    
-    it "should spell 66 as sechsundsechzig" do
-      test_number(66, 'sechsundsechzig')
-    end 
-  end
-  
-  describe "Seventies" do    
-    it "should spell 70 as siebzig" do
-      test_number(70, 'siebzig')
-    end
-    
-    it "should spell 77 as siebenundsiebzig" do
-      test_number(77, 'siebenundsiebzig')
-    end 
-  end
-  
-  describe "Eighties" do    
-    it "should spell 80 as achtzig" do
-      test_number(80, 'achtzig')
-    end
-    
-    it "should spell 88 as achtundachtzig" do
-      test_number(88, 'achtundachtzig')
-    end 
-  end
-  
-  describe "Nineties" do    
-    it "should spell 90 as neunzig" do
-      test_number(90, 'neunzig')
-    end
-    
-    it "should spell 99 as neunundneunzig" do
-      test_number(99, 'neunundneunzig')
-    end 
-  end
-
-  
-  describe "Hundreds" do
-    it "should spell 100 as einhundert" do
-      test_number(100, 'einhundert')
-    end
-    
-    it "should spell 101 as einhundertundeins" do
-      test_number(101, 'einhunderteins')
-    end
-    
-    it "should spell 111 as einhundertundelf" do
-      test_number(111, 'einhundertelf')
-    end
-    
-    it "should spell 155 as einhundertundfünfundfünfzig" do
-      test_number(155, 'einhundertfünfundfünfzig')
-    end
-    
-    it "should spell 190 as einhundertneunzig" do
-      test_number(190, 'einhundertneunzig')
-    end
-    
-    it "should spell 200 as zweihundert" do
-      test_number(200, 'zweihundert')
-    end
-    
-    it "should spell 222 as zweihundertzeiundzwanzig" do
-      test_number(222, 'zweihundertzweiundzwanzig')
-    end
-    
-    it "should spell 999 as neunhundertneunundneunzig" do
-      test_number(999, 'neunhundertneunundneunzig')
-    end
-  end
-
-  describe "Thousands" do
-    
-    it "should spell 1000 as eintausend" do
-      test_number(1000, 'eintausend')
-    end
-    
-    it "should spell 1001 as eintausendeins" do
-      test_number(1001, 'eintausendeins')
-    end
-    
-    it "should spell 1011 as eintausendelf" do
-      test_number(1011, 'eintausendelf')
-    end
-    
-    it "should spell 1100 as eintausendeinhundert" do
-      test_number(1100, 'eintausendeinhundert')
-    end
-    
-    it "should spell 1111 as eintausendeinhundertelf" do
-      test_number(1111, 'eintausendeinhundertelf')
-    end
-    
-    it "should spell 999,999 as neunhundertneunundneunzigtausendneunhundertneunundneunzig" do
-      test_number(999999, 'neunhundertneunundneunzigtausendneunhundertneunundneunzig')
-    end
-    
-  end
-
-
- describe "Millions" do
-   
-   it "should spell 1.000.000 as eine Million" do
-     test_number(1000000, 'eine Million')
-   end
-   
-   it "should spell 1.000.001 as eine Million und eins" do
-     test_number(1000001, 'eine Million und eins')
-   end
-   
-   it "should spell 1.000.011 as eine Million und elf" do
-     test_number(1000011, 'eine Million und elf')
-   end
-   
-   it "should spell 1.000.100 as eine Million und einhundert" do
-     test_number(1000100, 'eine Million und einhundert')
-   end
-   
-   it "should spell 1.000.111 as eine Million und einhundertelf" do
-     test_number(1000111, 'eine Million und einhundertelf')
-   end
-   
-   it "should spell 1.001.111 as eine Million und eintausendeinhundertelf" do
-     test_number(1001111, 'eine Million und eintausendeinhundertelf')
-   end
-   
-   it "should spell 999.999.999.999 as neunhundertneunundneunzigtausendneunundneunzig Millionen und neunhundertneunundneunzigtausendneunundneunzig" do
-     test_number(999999999999, 'neunhundertneunundneunzigtausendneunhundertneunundneunzig Millionen und neunhundertneunundneunzigtausendneunhundertneunundneunzig')
-   end
-   
- end
-
-
 end
--- spec/number_to_spell_spec.rb
+++ spec/number_to_spell_spec.rb
@@ -1,301 +1,105 @@
 require 'spell_number'
 
-describe "spell numbers in english" do  
-  
-  def test_number(number, result)
-    SpellNumber.number_to_words(number, :locale => 'en').should == result
-  end
-  
-  describe "Single digits" do
-    it "should transform 1" do
-      test_number(1, 'one')
-    end
-  
-    it "should transform 2" do
-      test_number(2, 'two')
-    end
-  
-    it "should transform 3" do
-      test_number(3, 'three')
-    end
-  
-    it "should transform 4" do
-      test_number(4, 'four')
-    end
-  
-    it "should transform 5" do
-      test_number(5, 'five')
-    end
-  
-    it "should transform 6" do
-      test_number(6, 'six')
-    end
-  
-    it "should transform 7" do
-      test_number(7, 'seven')
-    end
-  
-    it "should transform 8" do
-      test_number(8, 'eight')
-    end
-  
-    it "should transform 9" do
-      test_number(9, 'nine')
-    end
-  
-    it "should transform 0" do
-      test_number(0, 'zero')
-    end
-  end
-  
-  describe "Teens" do 
-    
-    it "should spell 10 as ten" do
-      test_number(10, 'ten')
-    end
-    
-    it "should spell 11 as eleven" do
-      test_number(11, 'eleven')
-    end
-    
-    it "should spell 12 as twelve" do
-      test_number(12, 'twelve')
-    end
-    
-    it "should spell 13 as thirteen" do
-      test_number(13, 'thirteen')
-    end
-    
-    it "should spell 14 as fourteen" do
-      test_number(14, 'fourteen')
-    end
-    
-    it "should spell 15 as fifteen" do
-      test_number(15, 'fifteen')
-    end
-    
-    it "should spell 16 as sixteen" do
-      test_number(16, 'sixteen')
-    end
-    
-    it "should spell 17 as seventeen" do
-      test_number(17, 'seventeen')
-    end
-    
-    it "should spell 18 as eighteen" do
-      test_number(18, 'eighteen')
-    end
-    
-    it "should spell 19 as nineteen" do
-      test_number(19, 'nineteen')
-    end
-  end
-  
-  describe "Twenties" do
-    
-    it "should spell 20 as twenty" do
-      test_number(20, 'twenty')
-    end
-    
-    it "should spell 21 as twenty-one" do
-      test_number(21, 'twenty-one')
-    end
-    
-    it "should spell 22 as twenty-two" do
-      test_number(22, 'twenty-two')
-    end
-    
-    it "should spell 23 as twenty-three" do
-      test_number(23, 'twenty-three')
-    end
-    
-    it "should spell 24 as twenty-four" do
-      test_number(24, 'twenty-four')
-    end
-    
-    it "should spell 25 as twenty-five" do
-      test_number(25, 'twenty-five')
-    end
-    
-    it "should spell 26 as twenty-six" do
-      test_number(26, 'twenty-six')
-    end
-    
-    it "should spell 27 as twenty-seven" do
-      test_number(27, 'twenty-seven')
-    end
-    
-    it "should spell 28 as twenty-eight" do
-      test_number(28, 'twenty-eight')
-    end
-    
-    it "should spell 29 as twenty-nine" do
-      test_number(29, 'twenty-nine')
+describe 'spell numbers in english' do
+  {
+    'Single digits' => {
+      1 => 'one',
+      2 => 'two',
+      3 => 'three',
+      4 => 'four',
+      5 => 'five',
+      6 => 'six',
+      7 => 'seven',
+      8 => 'eight',
+      9 => 'nine',
+      0 => 'zero'
+    },
+    'Teens' => {
+      10 => 'ten',
+      11 => 'eleven',
+      12 => 'twelve',
+      13 => 'thirteen',
+      14 => 'fourteen',
+      15 => 'fifteen',
+      16 => 'sixteen',
+      17 => 'seventeen',
+      18 => 'eighteen',
+      19 => 'nineteen'
+    },
+    'Twenties' => {
+      20 => 'twenty',
+      21 => 'twenty-one',
+      22 => 'twenty-two',
+      23 => 'twenty-three',
+      24 => 'twenty-four',
+      25 => 'twenty-five',
+      26 => 'twenty-six',
+      27 => 'twenty-seven',
+      28 => 'twenty-eight',
+      29 => 'twenty-nine'
+    },
+    'Thirties' => {
+      30 => 'thirty',
+      33 => 'thirty-three'
+    },
+    'Fourties' => {
+      40 => 'fourty',
+      44 => 'fourty-four'
+    },
+    'Fifties' => {
+      50 => 'fifty',
+      55 => 'fifty-five'
+    },
+    'Sixties' => {
+      60 => 'sixty',
+      66 => 'sixty-six'
+    },
+    'Seventies' => {
+      70 => 'seventy',
+      77 => 'seventy-seven'
+    },
+    'Eighties' => {
+      80 => 'eighty',
+      88 => 'eighty-eight'
+    },
+    'Nineties' => {
+      90 => 'ninety',
+      99 => 'ninety-nine'
+    },
+    'Hundreds' => {
+      100 => 'one hundred',
+      101 => 'one hundred and one',
+      111 => 'one hundred and eleven',
+      155 => 'one hundred and fifty-five',
+      190 => 'one hundred and ninety',
+      200 => 'two hundred',
+      222 => 'two hundred and twenty-two',
+      999 => 'nine hundred and ninety-nine'
+    },
+    'Thousands' => {
+      1000   => 'one thousand',
+      1001   => 'one thousand and one',
+      1011   => 'one thousand and eleven',
+      1100   => 'one thousand, one hundred',
+      1111   => 'one thousand, one hundred and eleven',
+      999999 => 'nine hundred and ninety-nine thousand, nine hundred and ninety-nine'
+    },
+    "Millions" => {
+      1000000      => 'one million',
+      1000001      => 'one million and one',
+      1000011      => 'one million and eleven',
+      1000100      => 'one million, one hundred',
+      1000111      => 'one million, one hundred and eleven',
+      1001111      => 'one million, one thousand, one hundred and eleven',
+      999999999999 => 'nine hundred and ninety-nine thousand, nine hundred and ninety-nine million, nine hundred and ninety-nine thousand, nine hundred and ninety-nine'
+    }
+  }.each_pair do |description, hash|
+    send(:describe, description) do
+      hash.each_pair do |number, result|
+        send(:it, "should spell #{number} as #{result}") {
+          SpellNumber.number_to_words(number, :locale => 'en').should == result
+        }
+      end
     end
   end
-  
-  describe "Thirties" do   
-    it "should spell 30 as thirty" do
-      test_number(30, 'thirty')
-    end
-    
-    it "should spell 33 as thirty-three" do
-      test_number(33, 'thirty-three')
-    end    
-  end
-  
-  describe "Fourties" do  
-    it "should spell 40 as fourty" do
-      test_number(40, 'fourty')
-    end
-    
-    it "should spell 44 as fourty-four" do
-      test_number(44, 'fourty-four')
-    end  
-  end
-  
-  describe "Fifties" do    
-    it "should spell 50 as fifty" do
-      test_number(50, 'fifty')
-    end
-    
-    it "should spell 55 as fifty-five" do
-      test_number(55, 'fifty-five')
-    end 
-  end
-  
-  describe "Sixties" do    
-    it "should spell 60 as sixty" do
-      test_number(60, 'sixty')
-    end
-    
-    it "should spell 66 as sixty-six" do
-      test_number(66, 'sixty-six')
-    end 
-  end
-  
-  describe "Seventies" do    
-    it "should spell 70 as seventy" do
-      test_number(70, 'seventy')
-    end
-    
-    it "should spell 77 as seventy-seven" do
-      test_number(77, 'seventy-seven')
-    end 
-  end
-  
-  describe "Eighties" do    
-    it "should spell 80 as eighty" do
-      test_number(80, 'eighty')
-    end
-    
-    it "should spell 88 as eighty-eight" do
-      test_number(88, 'eighty-eight')
-    end 
-  end
-  
-  describe "Nineties" do    
-    it "should spell 90 as ninety" do
-      test_number(90, 'ninety')
-    end
-    
-    it "should spell 99 as ninety-nine" do
-      test_number(99, 'ninety-nine')
-    end 
-  end
-  
-  describe "Hundreds" do
-    it "should spell 100 as one hundred" do
-      test_number(100,  'one hundred')
-    end
-    
-    it "should spell 101 as one hundred and one" do
-      test_number(101,  'one hundred and one')
-    end
-    
-    it "should spell 111 as one hundred and eleven" do
-      test_number(111, 'one hundred and eleven')
-    end
-    
-    it "should spell 155 as one hundred and fifty-five" do
-      test_number(155, 'one hundred and fifty-five')
-    end
-    
-    it "should spell 190 as one hundred and ninety" do
-      test_number(190, 'one hundred and ninety')
-    end
-    
-    it "should spell 200 as two hundred" do
-      test_number(200, 'two hundred')
-    end
-    
-    it "should spell 222 as two hundred and twenty-two" do
-      test_number(222, 'two hundred and twenty-two')
-    end
-    
-    it "should spell 999 as nine hundred and ninety-nine" do
-      test_number(999, 'nine hundred and ninety-nine')
-    end
-  end
-  
-  describe "Thousands" do
-    
-    it "should spell 1000 as one thousand" do
-      test_number(1000, 'one thousand')
-    end
-    
-    it "should spell 1001 as one thousand and one" do
-      test_number(1001, 'one thousand and one')
-    end
-    
-    it "should spell 1011 as one thousand and eleven" do
-      test_number(1011, 'one thousand and eleven')
-    end
-    
-    it "should spell 1100 as one thousand, one hundred" do
-      test_number(1100, 'one thousand, one hundred')
-    end
-    
-    it "should spell 1111 as one thousand, one hundred and one" do
-      test_number(1111, 'one thousand, one hundred and eleven')
-    end
-    
-    it "should spell 999,999 as nine hundred ninety-nine thousand, nine hundred and ninety-nine" do
-      test_number(999999, 'nine hundred and ninety-nine thousand, nine hundred and ninety-nine')
-    end
-    
-  end
-  
-  describe "Millions" do
-    
-    it "should spell 1.000.000 as one million" do
-      test_number(1000000, 'one million')
-    end
-    
-    it "should spell 1.000.001 as one million and one" do
-      test_number(1000001, 'one million and one')
-    end
-    
-    it "should spell 1.000.011 as one million and eleven" do
-      test_number(1000011, 'one million and eleven')
-    end
-    
-    it "should spell 1.000.100 as one million, one hundred" do
-      test_number(1000100, 'one million, one hundred')
-    end
-    
-    it "should spell 1.000.111 as one million, one hundred and eleven" do
-      test_number(1000111, 'one million, one hundred and eleven')
-    end
-    
-    it "should spell 1.001.111 as one million, one thousand, one hundred and eleven" do
-      test_number(1001111, 'one million, one thousand, one hundred and eleven')
-    end
-    
-    it "should spell 999.999.999.999 as nine hundred ninety-nine thousand million, nine hundred ninenty-nine thousand, nine hundred and ninety-nine" do
-      test_number(999999999999, 'nine hundred and ninety-nine thousand, nine hundred and ninety-nine million, nine hundred and ninety-nine thousand, nine hundred and ninety-nine')
-    end
-    
-  end
-  
 end
