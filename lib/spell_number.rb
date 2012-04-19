de616c004065b086f47e8577f65d485e5d9a141c
Johannes Opper <xijo@gmx.de>
2012-04-19 16:10:59 +0200

    Modifies i18n load paths to work well with rails3

--- lib/spell_number.rb
+++ lib/spell_number.rb
@@ -2,11 +2,11 @@ require 'spell_number/speller'
 require 'i18n'
 
 module SpellNumber
-  
-  I18n.load_path << Dir[File.join(File.dirname(__FILE__), '..', 'locales', '*.{rb,yml}').to_s]
-  
+
+  I18n.load_path += Dir[File.join(File.dirname(__FILE__), '..', 'locales', '*.{rb,yml}').to_s]
+
   def self.number_to_words(number, options = {})
     Speller.new(options).number_to_words(number)
   end
-  
+
 end
